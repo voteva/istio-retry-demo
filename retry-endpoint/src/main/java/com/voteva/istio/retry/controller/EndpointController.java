6c506c8be50ea49aa610d79f399a4feac7ae2b9d
voteva <tatianavoteva@gmail.com>
2020-06-10 15:26:00 +0300

    Update logging

--- retry-endpoint/src/main/java/com/voteva/istio/retry/controller/EndpointController.java
+++ retry-endpoint/src/main/java/com/voteva/istio/retry/controller/EndpointController.java
@@ -3,6 +3,7 @@ package com.voteva.istio.retry.controller;
 import com.voteva.istio.retry.service.HelperService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,17 @@ public class EndpointController {
     private final HelperService helperService;
 
     @RequestMapping("random")
-    public ResponseEntity<Mono<String>> withRandomResponse(@RequestParam(value = "durationMs", defaultValue = "0") long durationMs) {
+    public ResponseEntity<Mono<String>> withRandomResponse(
+            @RequestParam(value = "durationMs", defaultValue = "0", required = false) long durationMs) {
+
         int statusCode = helperService.getRandomStatusCode();
-        log.info("Path: '{}', Duration: '{}', Status code: '{}'", "random", durationMs, statusCode);
+
+        if (statusCode == HttpStatus.OK.value()) {
+            durationMs = 0;
+            log.info("Success. Path: '{}', Duration: '{}', Status code: '{}'\n", "random", durationMs, statusCode);
+        } else {
+            log.info("Error. Path: '{}', Duration: '{}', Status code: '{}'", "random", durationMs, statusCode);
+        }
         return ResponseEntity
                 .status(statusCode)
                 .body(Mono.just(format("DurationMs: %s, statusCode: %s", durationMs, statusCode))
@@ -31,10 +40,16 @@ public class EndpointController {
     }
 
     @RequestMapping(value = {"/", "/{path}"})
-    public ResponseEntity<Mono<String>> withStatus(@PathVariable(value = "path", required = false) String path,
-                                                   @RequestParam(value = "statusCode", defaultValue = "200") int statusCode,
-                                                   @RequestParam(value = "durationMs", defaultValue = "0") long durationMs) {
-        log.info("Path: '{}', DurationMs: '{}', StatusCode '{}'", path, durationMs, statusCode);
+    public ResponseEntity<Mono<String>> withStatus(
+            @PathVariable(value = "path", required = false) String path,
+            @RequestParam(value = "statusCode", defaultValue = "200", required = false) int statusCode,
+            @RequestParam(value = "durationMs", defaultValue = "0", required = false) long durationMs) {
+
+        if (statusCode == HttpStatus.OK.value()) {
+            log.info("Success. Path: '{}', Duration: '{}', Status code: '{}'\n", path, durationMs, statusCode);
+        } else {
+            log.info("Error. Path: '{}', DurationMs: '{}', StatusCode '{}'", path, durationMs, statusCode);
+        }
         return ResponseEntity
                 .status(statusCode)
                 .body(Mono.just(format("DurationMs: %s, statusCode: %s", durationMs, statusCode))
